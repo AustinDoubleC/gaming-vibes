55ceb2643e2ed2b3d9299d114d20282a3befb01b
Austin <austinccy81@gmail.com>
2022-07-13 17:26:11 +0100

    add styles

--- functions/guides.js
+++ functions/guides.js
@@ -5,13 +5,14 @@ exports.handler = async (event, context)=>{
         {title:"Ultimate Street Fighter Guide", author:"Chun-Li"}
     ]
     if (context.clientContext.user){
+    console.log("run")
     return{
         statusCode:200,
         body:JSON.stringify(guides)
     }
     }
-    /*return{
+    return{
         statusCode:401,
         body:JSON.stringify({mssg:"ah, ah you must be logged in to see this"})
-    }*/
+    }
 }
